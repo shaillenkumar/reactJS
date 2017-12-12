6f5b88de8824420e52c8f00d4b185ee5b55ec66c
shaillenkumar <shaillenkumar@gmail.com>
2017-12-12 11:06:18 +0530

    expesify app

---
+++ react-boilerplate/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/styles.scss';
+import 'normalize.css/normalize.css'
+
+
+console.log('App.js is running!');
+
+ReactDOM.render(<p>Boiler Plate</p>, document.getElementById('app'));
--- react-styles/src/app.js
+++ react-styles/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
+import 'normalize.css/normalize.css'
 
 
 console.log('App.js is running!');
